fe0c2d3baa57d0aa65c98d88cf688b2c351552de
sundaramrap <pragya.learn.28@gmail.com>
2024-11-05 14:23:20 +0530

    'part2VIdepFrontEnd'

---
+++ videogrid/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import TextField from "@mui/material/TextField";
+
+function SearchBar() {
+  return (
+    <div className="main">
+      <TextField variant="outlined" label="Search" />
+    </div>
+  );
+}
+
+export default SearchBar;
