91a7d8f169461b631c649465a37367fb98d78376
sundaramrap <pragya.learn.28@gmail.com>
2024-11-07 23:54:55 +0530

    'cardupadatedforFlag'

--- xcountryflag/src/Countries.jsx
+++ xcountryflag/src/Countries.jsx
@@ -1,23 +1,41 @@
+// card -->self made card
+// function which should have card in it and them we will map card in it and fetch data from the api through promise
+
 const Card = () => {
   return (
     <div
-      className="card"
-      style={{ width: "80px", height: "60px", border: "1px solid red" }}
+      className="XcountrtyFlag"
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        flexDirection: "row",
+        border: " 2px solid #ccc",
+        borderRadius: "20px",
+        alignItems: "center",
+        padding: "10px",
+        margin: "10px",
+        height: "120px",
+        width: "120px",
+      }}
     >
-      <div>
-        <img
-          src="https://cdn.pixabay.com/photo/2016/08/24/17/07/india-1617463_1280.png"
-          height={"30 px"}
-        />
-      </div>
+      <img
+        src="https://think360studio-media.s3.ap-south-1.amazonaws.com/photo/plugin/article/2021/india-flag-wallpaper-2021-1-12082021.png"
+        alt="alternative_name"
+        style={{ width: "50px", height: "50px" }}
+      />
+      <h5>Country Name</h5>
     </div>
   );
 };
 
 function Countries() {
+  const array = [1, 2, 3, 4, 5];
   return (
     <>
-      <Card />
+      {array.map((item) => (
+        <Card />
+      ))}
     </>
   );
 }
