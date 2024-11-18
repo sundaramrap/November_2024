e8d2dc89f11daddc6f64dbc890470a35f4aeb3ca
sundaramrap <pragya.learn.28@gmail.com>
2024-11-18 22:30:22 +0530

    note

--- nameplate/src/App.jsx
+++ nameplate/src/App.jsx
@@ -1,40 +1,16 @@
-import { useState } from "react";
-
 function App() {
-  const [fname, setFname] = useState();
-  const [lname, setLname] = useState();
-  const [cname, setCname] = useState();
-  const [showfname, setShowfname] = useState(false);
-
-  const handleSubmit = () => {
-    setCname(`${fname} ${lname}`);
-    setShowfname(true);
-  };
-
   return (
     <>
-      <h1>Full Name Display</h1>
-      <p>
-        First name:{" "}
-        <input
-          type="text"
-          value={fname}
-          onChange={(e) => setFname(e.target.value)}
-        />
-      </p>
-      <p>
-        Last name:{" "}
-        <input
-          type="text"
-          value={lname}
-          onChange={(e) => setLname(e.target.value)}
-        />
-      </p>
-      <button onClick={handleSubmit}>Submit</button>
-
-      {showfname && <p>Full Name:{cname}</p>}
+      <h1>Display Full Name</h1>
+      <form action="handleSubmit">
+        <label htmlFor="text">
+          First Name:
+          <input />
+        </label>
+        <br/>
+        
+      </form>
     </>
   );
 }
-
 export default App;
