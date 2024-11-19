33d5157f6d9814a696a60a1b9bba24082fad5801
sundaramrap <pragya.learn.28@gmail.com>
2024-11-19 23:02:19 +0530

    namep

--- nameplate/src/App.jsx
+++ nameplate/src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [fname, setFName] = useState("");
+
   return (
     <>
       <h1>Display Full Name</h1>
       <form action="handleSubmit">
         <label htmlFor="text">
-          First Name:
+          First Name:{fname}
           <input />
         </label>
-        <br/>
-        
+        <br />
+        <label>
+          Last Name: <input />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
       </form>
+      <p>FullName:</p>
     </>
   );
 }
